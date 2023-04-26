d579ae89a9050c22e4b78a0da738fc680bfe1329
Migo <mod200012@gmail.com>
2023-04-26 06:40:17 -0700

    print

--- print.c
+++ print.c
@@ -1,7 +1,13 @@
 #include "main.h"
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
 /**
  *_eputs - prints an input string
@@ -11,17 +17,21 @@ int _putchar(char c)
  */
 void _eputs(char *str)
 {
-	int i = 0;
-
-	if (!str)
-		return;
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
+int i = 0;
+if (!str)
+return;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
 }
- 
-void print_to_stderr(const char *message) {
-    write(STDERR_FILENO, message, strlen(message));
+}
+/**
+* print_to_stderr - writes a message to stderr
+* @message: The message to print
+* Return: Nothing
+*/
+void print_to_stderr(const char *message)
+{
+write(STDERR_FILENO, message, strlen(message));
 }
