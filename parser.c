b76c99aced541a3f54e3d33586ec448190095778
Migo <mod200012@gmail.com>
2023-04-26 06:33:56 -0700

    parser

--- parser.c
+++ parser.c
@@ -1,75 +1,49 @@
 #include "main.h"
-void dollar(char ** arg) {
-
-    int c;
-    for (c = 0; c < array_size(arg); c++) {
-        char *dollar = strfind(arg[c],'$',0);
-        char * tempo;
-        tempo = malloc(_strlen(_getenv("PATH") )* sizeof(char));
-        tempo = _getenv("PATH");
-        replace_all(arg[c],"$PATH", tempo); 
-
-        while (dollar != NULL ) {
-            int i;
-            char *var;
-            char *temp;
-            var = malloc(sizeof(char) * _strlen(dollar - 1));
-            for (i = 0; (i < _strlen(dollar)) && !(dollar[i] == '$' && i != 0); i++);
-            _strncpy(var, dollar, i);
-            if (_getenv(&var[1])) {
-                temp = malloc(_strlen(_getenv(&var[1])) * sizeof(char));
-                temp = _getenv(&var[1]);
-                replace_all(arg[c], var, temp);
-            } else {
-                replace_all(arg[c], var, "");
-            }
-
-            dollar = strfind(arg[c], '$', dollar - arg[c] + i);
-        }
-    }
-}
-void parse (char ** args)
+/**
+ * dollar - replaces environment variables with their value
+ * @arg: pointer to array of strings containing command and its arguments
+ *
+ * Return: void
+ */
+void dollar(char **arg)
 {
-    dollar(args);
+int c;
+for (c = 0; c < array_size(arg); c++)
+{
+char *dollar = strfind(arg[c], '$', 0);
+char *tempo;
+tempo = malloc(_strlen(_getenv("PATH")) * sizeof(char));
+tempo = _getenv("PATH");
+replace_all(arg[c], "$PATH", tempo);
+while (dollar != NULL)
+{
+int i;
+char *var;
+char *temp;
+var = malloc(sizeof(char) * _strlen(dollar - 1));
+for (i = 0; (i < _strlen(dollar)) && !(dollar[i] == '$' && i != 0); i++)
+_strncpy(var, dollar, i);
+if (_getenv(&var[1]))
+{
+temp = malloc(_strlen(_getenv(&var[1])) * sizeof(char));
+temp = _getenv(&var[1]);
+replace_all(arg[c], var, temp);
 }
-/*
-                        .-""""-.
-                       / j      \
-                      :.d;       ;
-                      $$P        :
-           .m._       $$         :
-          dSMMSSSss.__$$b.    __ :
-         :MMSMMSSSMMMSS$$$b  $$P ;
-         SMMMSMMSMMMSSS$$$$     :b
-        dSMMMSMMMMMMSSMM$$$b.dP SSb.
-       dSMMMMMMMMMMSSMMPT$$=-. /TSSSS.
-      :SMMMSMMMMMMMSMMP  `$b_.'  MMMMSS.
-      SMMMMMSMMMMMMMMM \  .'\    :SMMMSSS.
-     dSMSSMMMSMMMMMMMM  \/\_/; .'SSMMMMSSSm
-    dSMMMMSMMSMMMMMMMM    :.;'" :SSMMMMSSMM;
-  .MMSSSSSMSSMMMMMMMM;    :.;   MMSMMMMSMMM;
- dMSSMMSSSSSSSMMMMMMM;    ;.;   MMMMMMMSMMM
-:MMMSSSSMMMSSP^TMMMMM     ;.;   MMMMMMMMMMM
-MMMSMMMMSSSSP   `MMMM     ;.;   :MMMMMMMMM;
-"TMMMMMMMMMM      TM;    :`.:    MMMMMMMMM;
-   )MMMMMMM;     _/\\    :`.:    :MMMMMMMM
-  d$SS$$$MMMb.  |._\\\   :`.:     MMMMMMMM
-  T$$S$$$$$$$$$$m;O\\\\"-;`.:_.-  MMMMMMM;
- :$$$$$$$$$$$$$$$b_l./\\ ;`.:    mMMSSMMM;
- :$$$$$$$$$$$$$$$$$$$./\\;`.:  .$$MSMMMMMM
-  $$$$$$$$$$$$$$$$$$$$. \\`.:.$$$$SMSSSMMM;
-  $$$$$$$$$$$$$$$$$$$$$. \\.:$$$$$SSMMMMMMM
-  :$$$$$$$$$$$$$$$$$$$$$.//.:$$$$SSSSSSSMM;
-  :$$$$$$$$$$$$$$$$$$$$$$.`.:$$SSSSSSSMMMP
-   $$$$$$$$$;"^$J "^$$$$;.`.$$P  `SSSMMMM
-   :$$$$$$$$$       :$$$;.`.P'..   TMMM$$b
-   :$$$$$$$$$;      $$$$;.`/ c^'   d$$$$$S;
-   $$$$$S$$$$;      '^^^:_d$g:___.$$$$$$SSS
-   $$$$SS$$$$;            $$$$$$$$$$$$$$SSS;
-  :$$$SSSS$$$$            : $$$$$$$$$$$$$SSS
-  :$P"TSSSS$$$            ; $$$$$$$$$$$$$SSS;
-  j    `SSSSS$           :  :$$$$$$$$$$$$$SS$
- :       "^S^'           :   $$$$$$$$$$$$$S$;
- ;.____.-;"               "--^$$$$$$$$$$$$$P
- '-....-"  bug                  ""^^T$$$$P"
-*/
+else
+{
+replace_all(arg[c], var, "");
+}
+dollar = strfind(arg[c], '$', dollar - arg[c] + i);
+}
+}
+}
+/**
+ * parse - calls necessary functions to parse and execute a command
+ * @args: pointer to array of strings containing command and its arguments
+ *
+ * Return: void
+ */
+void parse(char **args)
+{
+dollar(args);
+}
