161fab33606c30917c4e0a09767fc69a0654fc19
philopaterwaheed <philopaterwaheed9@gmail.com>
2023-04-26 14:48:04 -0700

    betty

--- strcpy.c
+++ strcpy.c
@@ -66,31 +66,35 @@ char *_memcpy(char *dest, char *src, unsigned int n)
  */
 void *_memmove(void *dest, const void *src, size_t n)
 {
-    char *d = dest;
-    const char *s = src;
+	char *d = dest;
+	const char *s = src;
 
-    if (d == s) {
-        return d;
-    }
+	if (d == s)
+	{
+		return (d);
+	}
 
-    if (s + n <= d || d + n <= s) {
-        /* No overlap, use memcpy */
-        return memcpy(d, s, n);
-    }
+	if (s + n <= d || d + n <= s)
+	{
+		return (memcpy(d, s, n));
+	}
 
-    if (d < s) {
-        /* Copy from start to end */
-        while (n--) {
-            *d++ = *s++;
-        }
-    } else {
-        /* Copy from end to start */
-        d += n - 1;
-        s += n - 1;
-        while (n--) {
-            *d-- = *s--;
-        }
-    }
+	if (d < s)
+	{
+		while (n--)
+		{
+			*d++ = *s++;
+		}
+	}
+else
+{
+		d += n - 1;
+		s += n - 1;
+		while (n--)
+		{
+			*d-- = *s--;
+		}
+	}
 
-	return dest;
+	return (dest);
 }
