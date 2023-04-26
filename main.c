94810ec79469660aca4e4b4614065e6df714e097
Migo <mod200012@gmail.com>
2023-04-26 06:20:40 -0700

    main

--- main.c
+++ main.c
@@ -1,57 +1,26 @@
 #include "main.h"
-
+/**
+ * main - Entry point for the shell program
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    while (1)
-    {
-        char *line = "exit";
-        char **arguments;
-        line = _getline();
-        if (line != NULL)
-        {
-            arguments = token(line);
-            parse(arguments);
-            exe (arguments);
-            free(line);
-            free(arguments);
-        }   
-        else 
-            continue;
-    }
-    return (0);
+while (1)
+{
+char *line = "exit";
+char **arguments;
+line = _getline();
+if (line != NULL)
+{
+arguments = token(line);
+parse(arguments);
+exe(arguments);
+free(line);
+free(arguments);
 }
-/*
-          .sd$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$;             
- [bug]  .d$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$             
-      .d$$$$$$$$$$P*"'   `"*T$$$$$$$$$$$$$$$             
-     s$$$$$$$$$P*             `*T$$$$$$$$$$$             
-    d$$$$$$$$P'                  `*T$$$$$$$P             
-   d$$$$$$$P'                       `T$$$$P              
-  d$$$$$$P'                           `T$P \             
- d$$$$$P'                                   \            
-.$$$$$'                 .*"*                 .           
-:$$$$;                  .*"*-.                           
-$$$$$'                 /                      `          
-$$$$$.                  .s$$s.    `*.         :          
-$$$$$;                 d$$$$$$b      ;     .s$s.         
-$$$$$$b.              d$$$$$$$$b     ;    d$$$$$b        
-$$$$$$$$$bs._        d$$$$P^^T$$b   /    d$P"""T$b       
-$$$$$$$$$$$$$$bs+=- .$$P*'    `TP       dP     .`T       
-$$$$$$$$$$$$$$$$P'  :P'    __          /                 
-$$$$$$$$$$$$$$$P    $    .'  `.       /`-.    .          
-T$$$$$$$$$$$$$$     :   /      \     .    `.             
- T$$$$$$$$$$$$$        :        ;           \.           
-  T*'   `*^$$$$        |+*"$P*sss*"  :*"$P*ss*"          
- /         `T$$        |   Tbd$P     :  Tbd$P            
-;  ._        T$        |    T$$P     :   T$P      _._    
-     `"*+.    T       *"**--._/       \   /`. .-*"   `*. 
-     .*'  `.                  `*       `*---*'          ;
-    (                              `.                   |
-                   .'                \                  ;
-\           /     /`*+...___          `-.             .* 
- `-._   _.+'          `*. __"""****------`*-.____.+*"'   
-     "*"   \             `. """****------**"/;           
-            `.         \   `--..._______...'/            
-              `*--..___.`.                 /             
-                          `*-...______..-*'
-*/
+else
+continue;
+}
+return (0);
+}
--- main.h
+++ main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
@@ -7,6 +10,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <stdarg.h>
+
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
@@ -14,55 +18,45 @@
 #define MAX_COMMAND_LENGTH 1024
 #define MAX_ARGUMENTS 64
 #define MAX_ENV_VARIABLES 1024
+
 extern char **environ;
+
 int _putchar(char c);
-char* _getline();
-char** token(char* line);
-void exe(char ** arguments);
+char *_getline();
+char **token(char *line);
+void exe(char **arguments);
 char *location(char *command);
-void env(void* argument);
-void spexe(int index, void* arg);
-int sp(char * argument);
-void echo(void * arguments);
+void env(void *argument);
+void spexe(int index, void *arg);
+int sp(char *argument);
+void echo(void *arguments);
 void __exit(void *argument);
-char* strfind(char* , char, int);
-int* ctoi(char * );
+char *strfind(char *s, char c, int n);
+int *ctoi(char *);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strdup(char *s);
 char *_memcpy(char *dest, char *src, unsigned int n);
-int _strcmp(const char* str1, const char* str2);
+int _strcmp(const char *str1, const char *str2);
 char *_strncpy(char *dest, char *src, int n);
-void str_replace(char *str, const char *substr, const char *new_substr, size_t start, size_t end);
+void str_replace(char *str, const char *substr, const char
+*new_substr, size_t start, size_t end);
 int _strncmp(const char *s1, const char *s2, size_t n);
-char * _strappened(char* str, char ch);
+char *_strappened(char *str, char ch);
 void replace_substr(char *str, char *substr, char *replace);
-char* _getenv(char* name);
-void _eputs(char *);
+char *getenv(char *name);
+void eputs(char *);
 int array_size(char **array);
-void parse (char ** args);
-void dollar(char ** args);
-void cd (void* argument);
-void setenv_(void* argument);
-void print_to_stderr(const char *message) ;
+void parse(char **args);
+void dollar(char **args);
+void cd(void *argument);
+void setenv(void *argument);
+void print_to_stderr(const char *message);
 void replace_all(char *str, char *substr, char *replace);
-void unsetenv_(void* argument);
-char *_strchr( char *str, int ch);
-int  set_env_variable(char *name, char *value, int overwrite) ;
+void unsetenv(void *argument);
+char *_strchr(char *str, int ch);
+int set_env_variable(char *name, char *value, int overwrite);
 int _unsetenv(char *name);
-/*
-       \:.             .:/
-        \``._________.''/ 
-         \             / 
- .--.--, / .':.   .':. \
-/__:  /  | '::' . '::' |
-   / /   |`.   ._.   .'|
-  / /    |.'         '.|
- /___-_-,|.\  \   /  /.|
-      // |''\.;   ;,/ '|
-      `==|:=         =:|
-         `.          .'
-l42        :-._____.-:
-          `''       `''
-*/
+
+#endif
