09b072921d4aa1ca305601e579f72559503b94ed
Migo <mod200012@gmail.com>
2023-04-26 06:08:55 -0700

    need description

--- exe.c
+++ exe.c
@@ -1,61 +1,51 @@
 #include "main.h"
-void echo(void * arguments)
+void echo(void *arguments)
 {
-    (void) arguments;
+(void) arguments;
 }
-
-void exe(char ** arguments)
+void exe(char **arguments)
 {
-    pid_t pid;
-    char* command = NULL , *excommand;
-    int status;
-    
-    if (sp(arguments[0]) != -1){
-        spexe(sp(arguments[0]), arguments);
-        return;
-    }
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-    }
-    else if (pid ==0 ) {
-        if (arguments)
-        {
-            command = arguments[0] ;         
-            excommand = location(command);
-            if (execve(excommand, arguments, NULL) == -1)
-            {
-                perror(arguments[0]);
-            };
-        }
-    }
-     else {
-        waitpid (pid, &status, 0); 
-     }
+pid_t pid;
+char *command = NULL, *excommand;
+int status;
+if (sp(arguments[0]) != -1)
+{
+spexe(sp(arguments[0]), arguments);
+return;
 }
-int sp(char * argument)
+pid = fork();
+if (pid == -1)
 {
-    int i;
-    char*  sp [] = {"env" ,"exit","cd","setenv","unsetenv" };
-    for (i = 0; i < 5;i++)
-        if(_strcmp(argument,sp[i]) == 0)
-            return i;
-    return -1;
-
+perror("fork");
 }
-void spexe(int index, void* arg)
+else if (pid == 0)
 {
-    void (*fun[]) (void*) = {env,__exit,cd,setenv_,unsetenv_};
-    (*fun[index])(arg);
-/*
-    ╔╗    ╔╗                                         
-    ║║    ║║                                         
-╔══╗║╚═╗╔╗║║ ╔══╗    ╔╗╔══╗    ╔══╗╔╗╔═╗ ╔╗╔╗╔══╗╔══╗
-║╔╗║║╔╗║╠╣║║ ║╔╗║    ╠╣║══╣    ║╔╗║╠╣║╔╗╗║║║║║╔╗║║══╣
-║╚╝║║║║║║║║╚╗║╚╝║    ║║╠══║    ║╚╝║║║║║║║║╚╝║║║═╣╠══║
-║╔═╝╚╝╚╝╚╝╚═╝╚══╝    ╚╝╚══╝    ╚═╗║╚╝╚╝╚╝╚══╝╚══╝╚══╝
-║║                             ╔═╝║                  
-╚╝                             ╚══╝                  
-*/
+if (arguments)
+{
+command = arguments[0];
+excommand = location(command);
+if (execve(excommand, arguments, NULL) == -1)
+{
+perror(arguments[0]);
+};
+}
+}
+else
+{
+waitpid (pid, &status, 0);
+}
+}
+int sp(char *argument)
+{
+int i;
+char *sp [] = {"env", "exit", "cd", "setenv", "unsetenv"};
+for (i = 0; i < 5; i++)
+if (_strcmp(argument, sp[i]) == 0)
+return (i);
+return (-1);
+}
+void spexe(int index, void *arg)
+{
+void (*fun[]) (void *) = {env, __exit, cd, setenv_, unsetenv_};
+(*fun[index])(arg);
 }
